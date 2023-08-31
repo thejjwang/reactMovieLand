564e3ad48a3657b6469f649d66fe3c037a8305b4
thejjwang <jonathanjacobwang@gmail.com>
2023-08-30 18:35:12 -0700

    dynamic search working

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,6 +6,7 @@ import MovieCard from "./MovieCard";
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=33bbe8bc";
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -32,13 +33,13 @@ const App = () => {
       <h1>MovieLand</h1>
 
       <div className="search">
-        <input placeholder="Search for movies" value="" onChange={() => {}} />
-        <img src={SearchIcon} alt="search" onClick={() => {}} />
+        <input placeholder="Search for movies" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        <img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
       </div>
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie}/>
+            <MovieCard key={movie.imdbID} movie={movie}/>
           ))}
         </div>
       ) : (
